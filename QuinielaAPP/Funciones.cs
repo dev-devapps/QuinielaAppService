a9e7617f22f2c4958b626ec784f0ba97a05ac1ac
Alfredo Morales <alfredo.57pro@gmailcom>
2018-04-19 20:39:40 -0600

    Aplicacion para actualizacion

---
+++ QuinielaAPP/Funciones.cs
@@ -0,0 +1,168 @@
+﻿// Funciones.cs
+using System;
+using System.Net.Mail;
+
+namespace QuinielaAPP
+{
+    public class Funciones
+    {
+        public Funciones()
+        {
+        }
+    }
+
+    public class SMTPClass
+    {
+        public String HOST { get; set; }
+        public int PORT { get; set; }
+        public String SMTP_USERNAME { get; set; }
+        public String SMTP_PASSWORD { get; set; }
+        public Boolean ENABLESSL { get; set; }
+
+        public SMTPClass()
+        {
+            SMTP_USERNAME = "";
+            SMTP_PASSWORD = "";
+            ENABLESSL = false;
+        }
+
+        /// <summary>Método que envía un correo electrónico a un destinatario o destinatarios, teniendo la opción de enviar archivos adjuntos y/o código HTML.</summary>
+        /// <param name="strDisplayName">Nombre de quien está enviando el correo electrónico.</param>
+        /// <param name="strFrom">Acá se coloca el correo electrónico de quien está originando el correo</param>
+        /// <param name="strTo">Correo electrónico lista de correos electrónicos a quienes se enviará el mensaje, para enviar a más de un destinatario, se debe de serparar por comas. 
+        /// <example>Ejemplo: pepito@correo.com,mafalda@correo.com</example>
+        /// </param>
+        /// <param name="strCc">Listado de correos (separados por comas) a quienes se les enviará la copia.</param>
+        /// <param name="strBcc">Listado de correos (separados por comas) a quienes se les enviará la copia oculta.</param>
+        /// <param name="strSubject">Asunto del correo electrónico.</param>
+        /// <param name="isHTML">Especifica se si enviará texto plano o código HTML, para texto plano enviar "false", para enviar código HTML se debe de enviar "true".</param>
+        /// <param name="strBody">Contenido del correo eléctrónico.</param>
+        /// <param name="strAttachmentPath">Ruta física en donde se encuentra el archivo, si no se desea adjuntar archivo enviar vacío. Para enviar más de un adjunto separar por comas las rutas.</param>
+        public String SendMail(String strDisplayName, String strFrom, String strTo, String strCc, String strBcc, String strSubject, Boolean isHTML, String strBody, String strAttachmentPath)
+        {
+            String lResult = "";
+            String strErrores = "";
+            String strResultado = "";
+            String strAttachments = "";
+            String strAttachDetail = "";
+
+            try
+            {
+                MailAddress mailFrom = new MailAddress(strFrom, strDisplayName);
+                MailAddressCollection mailTo = new MailAddressCollection();
+                System.Net.Mail.MailMessage message = new System.Net.Mail.MailMessage();
+                mailTo.Add(strTo);
+
+                System.Net.Mail.SmtpClient client = new System.Net.Mail.SmtpClient(HOST, PORT);
+
+                if (!SMTP_USERNAME.Equals(""))
+                    client.Credentials = new System.Net.NetworkCredential(SMTP_USERNAME, SMTP_PASSWORD);
+
+                client.EnableSsl = ENABLESSL;
+
+                //Asignacion de variables
+                message.IsBodyHtml = isHTML;
+                message.From = mailFrom;
+
+                message.To.Add(strTo);
+                message.Subject = strSubject;
+                message.Body = strBody;
+
+                //Copia
+                if (!strCc.Equals(""))
+                {
+                    message.CC.Add(strCc);
+                }
+
+                //Copia oculta
+                if (!strBcc.Equals(""))
+                {
+                    message.Bcc.Add(strBcc);
+                }
+
+                if (!strAttachmentPath.Equals(""))
+                {
+
+                    String[] arrFileName;
+
+                    arrFileName = strAttachmentPath.Split(',');
+
+                    if (arrFileName.Length > 0)
+                    {
+                        for (int i = 0; i < arrFileName.Length; i++)
+                        {
+                            try
+                            {
+                                message.Attachments.Add(new Attachment(arrFileName[i]));
+
+                                strAttachDetail += "<adjunto resultado=\"1\">" +
+                                                        "<mensaje>OK</mensaje>" +
+                                                        "<path><![CDATA[" + arrFileName[i] + "]]></path>" +
+                                                   "</adjunto>";
+                            }
+                            catch (Exception e)
+                            {
+                                strErrores += e.Message + ": " + arrFileName[i] + "|";
+
+                                strAttachDetail += "<adjunto resultado=\"2\">" +
+                                                        "<mensaje><![CDATA[" + e.Message + "]]></mensaje>" +
+                                                        "<path><![CDATA[" + arrFileName[i] + "]]></path>" +
+                                                   "</adjunto>";
+                            }
+                        }
+                    }
+                    else
+                    {
+                        try
+                        {
+                            Attachment adjunto = new Attachment(strAttachmentPath);
+                            message.Attachments.Add(adjunto);
+
+                            strAttachDetail += "<adjunto resultado=\"1\">" +
+                                                    "<mensaje>OK</mensaje>" +
+                                                    "<path><![CDATA[" + strAttachmentPath + "]]></path>" +
+                                               "</adjunto>";
+                        }
+                        catch (Exception e2)
+                        {
+                            strErrores += e2.Message + ": " + strAttachmentPath + "|";
+
+                            strAttachDetail += "<adjunto resultado=\"2\">" +
+                                                    "<mensaje><![CDATA[" + e2.Message + "]]></mensaje>" +
+                                                    "<path><![CDATA[" + strAttachmentPath + "]]></path>" +
+                                               "</adjunto>";
+                        }
+                    }
+
+                    strAttachments = "<adjuntos>" +
+                                        strAttachDetail +
+                                     "</adjuntos>";
+
+                }
+                else
+                {
+                    strAttachments = "<adjuntos/>";
+                }
+
+                client.Send(message);
+                lResult = "1 - Email sent!";
+
+                strResultado = "<envio_correo>" +
+                                    "<resultado codigo=\"1\">Envio de correo exitoso.</resultado>" +
+                                    strAttachments +
+                               "</envio_correo>";
+            }
+            catch (Exception ex)
+            {
+                lResult = "2 - The email was not sent. Error message: " + ex.Message;
+
+                strResultado = "<envio_correo>" +
+                                    "<resultado codigo=\"2\"><![CDATA[" + ex.Message + "]]></resultado>" +
+                               "</envio_correo>";
+            }
+
+            //return lResult;
+            return strResultado;
+        }
+    }
+}
---
+++ QuinielaAPP/Program.cs
@@ -0,0 +1,320 @@
+﻿// Program.cs
+using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+
+namespace QuinielaAPP
+{
+    class MainClass
+    {
+        public const string SQLCONNSTRING = "SQLConnString";
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Iniciando...");
+
+
+            CambiaEstadoPartidos();
+
+            ConsultaPartidoFinalizado();
+
+
+            Console.ReadKey(true);
+        }
+
+
+        private static void CambiaEstadoPartidos(){
+            try
+            {
+                Console.Write("Actualizando partidos... ");
+                // Build connection string
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.ConnectionString = ConfigurationManager.ConnectionStrings[SQLCONNSTRING].ConnectionString;
+
+                // Connect to SQL
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    connection.Open();
+
+                    string query = "EXEC sp_cambia_estado_partido";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.ExecuteNonQuery();
+                        Console.WriteLine("Actualizacion exitosa! :)");
+                    }
+
+                    query = "select pa_id, E1.eq_descripcion 'Equipo1', E2.eq_descripcion 'Equipo1' from Partido, Equipo as E1, Equipo as E2 where pa_estado = 'C' and E1.eq_id = pa_idEquipo1 and E2.eq_id = pa_idEquipo2 and pa_hora_pronostico is null";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            int idPartido = 0;
+                            string equipo1 = "", equipo2 = "", htmlCuerpoCorreo = "";
+
+
+                            while (reader.Read())
+                            {
+                                idPartido = reader.GetInt32(0);
+                                equipo1 = reader.GetString(1);
+                                equipo2 = reader.GetString(2);
+
+                                htmlCuerpoCorreo = ArmaHTMLPronostico(idPartido, equipo1, equipo2);
+
+                                //EnvioCorreo("Pronosticos " + equipo1 + " vs. " + equipo2, htmlCuerpoCorreo);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("Ocurrio un error actualizando los partidos:" + e.ToString());
+            }
+        }
+
+        private static string ArmaHTMLPronostico(int idPartido, string equipo1, string equipo2)
+        {
+            string htmlPronosticos = "", query = "", trClass = "";
+            int cont = 0;
+
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.ConnectionString = ConfigurationManager.ConnectionStrings[SQLCONNSTRING].ConnectionString;
+
+            // Connect to SQL
+            using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+            {
+                connection.Open();
+
+                query = "EXEC sp_tabla_pronostico @idPartido";
+
+
+                using (SqlCommand cmMarcador = new SqlCommand(query, connection))
+                {
+
+                    cmMarcador.Parameters.AddWithValue("@idPartido", idPartido);
+                    SqlDataReader rMarcadores = cmMarcador.ExecuteReader();
+
+                    htmlPronosticos = "<table align=\"center\" cellpadding=\"2\" cellspacing=\"2\" border=\"0\">" +
+                      "<tr style=\"font-family:Verdana;font-size:11px;font-weight: bold;padding:0px 10px 0px 0px;color:#FFFFFF;background-color:#0B0B3B;\">" +
+                        "<td align=\"center\">Alias</td>" +
+                        "<td align=\"center\">" + equipo1 + "</td>" +
+                        "<td align=\"center\">" + equipo2 + "</td>" +
+                        "<td align=\"center\">Fecha</td>" +
+                      "</tr>";
+
+                    while (rMarcadores.Read())
+                    {
+
+                        if ((cont % 2) == 0)
+                        {
+                            trClass = "font-family:Verdana;font-size:11px;padding:0px 5px 0px 0px;color:#FFFFFF;background-color:#1E5E9E;";
+                        }
+                        else
+                        {
+                            trClass = "font-family:Verdana;font-size:11px;padding:0px 5px 0px 0px;color:#FFFFFF;background-color:#58ACFA;";
+                        }
+
+                        htmlPronosticos += "<tr style=\"" + trClass + "\">" +
+                                                   "<td>" + rMarcadores.GetString(1) + "</td>" +
+                                                   "<td align=\"center\">" + (rMarcadores.GetInt32(2) == -1 ? "-" : rMarcadores.GetInt32(2).ToString()) + "</td>" +
+                                                   "<td align=\"center\">" + (rMarcadores.GetInt32(3) == -1 ? "-" : rMarcadores.GetInt32(3).ToString()) + "</td>" +
+                                                   "<td align=\"center\">" + (rMarcadores.GetInt32(3) == -1 ? "-" : rMarcadores.GetString(4) + " " + rMarcadores.GetString(5)) + "</td>" +
+                                            "</tr>";
+
+                        cont++;
+                    }
+
+                    htmlPronosticos += "</table>";
+                }
+
+                query = "update Partido set pa_hora_pronostico = GETDATE() where pa_id = @idPartido";
+
+                using(SqlCommand cmActualiza = new SqlCommand(query, connection)){
+                    cmActualiza.Parameters.AddWithValue("@idPartido", idPartido);
+
+                    cmActualiza.ExecuteNonQuery();
+                }
+            }
+
+
+
+            return htmlPronosticos;
+        }
+
+        private static void ConsultaPartidoFinalizado(){
+            try
+            {
+                Console.Write("Verificando si existen partidos finalizados... ");
+                // Build connection string
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.ConnectionString = ConfigurationManager.ConnectionStrings[SQLCONNSTRING].ConnectionString;
+
+                // Connect to SQL
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    connection.Open();
+
+                    string query = "select pa_id from Partido where pa_estado = 'T' and pa_hora_ranking is null";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            int idPartido = 0;
+                            string htmlCuerpoCorreo = "";
+
+
+                            while (reader.Read())
+                            {
+                                idPartido = reader.GetInt32(0);
+
+                                htmlCuerpoCorreo = ArmaHTMLRanking();
+
+                                //EnvioCorreo("Ranking Quiniela " + DateTime.Now.ToString(), htmlCuerpoCorreo);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("Ocurrio un error actualizando los partidos:" + e.ToString());
+            }
+        }
+
+        private static string ArmaHTMLRanking()
+        {
+            string htmlPronosticos = "", query = "", trClass = "";
+            int cont = 0, ranking = 1, puntosAnt = 0;
+
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.ConnectionString = ConfigurationManager.ConnectionStrings[SQLCONNSTRING].ConnectionString;
+
+            // Connect to SQL
+            using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+            {
+                connection.Open();
+
+                query = "EXEC sp_tabla_ranking";
+
+                using (SqlCommand cmMarcador = new SqlCommand(query, connection))
+                {
+                    SqlDataReader rMarcadores = cmMarcador.ExecuteReader();
+
+                    htmlPronosticos = "<table align=\"center\" cellpadding=\"2\" cellspacing=\"2\" border=\"0\">" +
+                      "<tr style=\"font-family:Verdana;font-size:11px;font-weight: bold;padding:0px 10px 0px 0px;color:#FFFFFF;background-color:#0B0B3B;\">" +
+                        "<td align=\"center\">No.</td>" +
+                        "<td align=\"center\">Posici&oacute;n</td>" +
+                        "<td align=\"center\">Alias</td>" +
+                        "<td align=\"center\">Puntos</td>" +
+                      "</tr>";
+
+                    while (rMarcadores.Read())
+                    {
+
+                        if ((cont % 2) == 0)
+                        {
+                            trClass = "font-family:Verdana;font-size:11px;padding:0px 5px 0px 0px;color:#FFFFFF;background-color:#1E5E9E;";
+                        }
+                        else
+                        {
+                            trClass = "font-family:Verdana;font-size:11px;padding:0px 5px 0px 0px;color:#FFFFFF;background-color:#58ACFA;";
+                        }
+
+                        if(rMarcadores.GetInt32(1) == puntosAnt){
+                            puntosAnt = rMarcadores.GetInt32(1);
+                            ranking++;
+                        }
+
+                        htmlPronosticos += "<tr style=\"" + trClass + "\">" +
+                                                   "<td>" + (cont + 1) + "</td>" +
+                                                   "<td align=\"center\">" + ranking + "</td>" +
+                                                   "<td align=\"center\">" + rMarcadores.GetString(0) + "</td>" +
+                                                   "<td align=\"center\">" + rMarcadores.GetInt32(1) + "</td>" +
+                                            "</tr>";
+
+                        cont++;
+                    }
+
+                    htmlPronosticos += "</table>";
+                }
+
+                query = "update Partido set pa_hora_ranking = GETDATE() where pa_estado = 'T' and pa_hora_ranking is null";
+
+                using (SqlCommand cmActualiza = new SqlCommand(query, connection))
+                {
+                    cmActualiza.ExecuteNonQuery();
+                }
+            }
+
+
+
+            return htmlPronosticos;
+        }
+
+        private static void EnvioCorreo(string asunto, string cuerpoMensaje){
+            SMTPClass mail = new SMTPClass();
+            string resEnvioMail = "", query = "", correo = "", mensaje = "";
+
+            mail.HOST = "";
+            mail.PORT = 25;
+
+            mensaje = "<html>" +
+                                "<head>" +
+                                    "<title>Quiniela</title>" +
+                                "</head>" +
+                                "<body>" +
+                                cuerpoMensaje +
+                                "</body>" +
+                       "</html>";
+
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.ConnectionString = ConfigurationManager.ConnectionStrings[SQLCONNSTRING].ConnectionString;
+
+            // Connect to SQL
+            using (SqlConnection connection = new SqlConnection(builder.ConnectionString)){
+                connection.Open();
+
+                query = "select us_correoElectronico from Usuario";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            correo = reader.GetString(0);
+
+                            resEnvioMail = mail.SendMail("Quiniela Rusia 2018", "info@devapps.com", correo, "", "", asunto, true, mensaje, "");
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /*class Pronostico{
+        public string alias { get; set; }
+
+        public int id_alias { get; set; }
+
+        public int marcador1 { get; set; }
+
+        public int marcador2 { get; set; }
+
+        public int pa_marcador1 { get; set; }
+
+        public int pa_marcador2 { get; set; }
+
+        public string puntos { get; set; }
+
+        public string hora { get; set; }
+
+        public int CalculaPuntos(){
+            
+        }
+    }*/
+}
